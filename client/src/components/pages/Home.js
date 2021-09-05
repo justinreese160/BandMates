fe2b56a1ed0d222e0cf6e8217b273f49feb605b7
Zhansaya Sarmanova <zhansayasarmanova1993@gmail.com>
2021-09-05 13:24:03 -0500

    added home page image

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -11,6 +11,7 @@ function Home() {
     <img style={{ backgroundPosition: "center", backgroundSize: "cover", borderBottomStyle: "outset", margin: "0 0 -5 0" }} className="img" src="https://i.imgur.com/US1bcVM.jpg"></img>
           
        
+        
       </Jumbotron>
     {/* <img style={{background:"https://i.imgur.com/zDBO5ot.jpg",margin:"5% 25%",border:"solid #e6dcca ",maxWidth:"600px", width: "100%", maxHeight:"auto"}} className="home-img" src="https://i.imgur.com/cRCyN5v.png"  alt="BandMates logo"></img>  */}
       <h1 className="about" style={{ textShadow:"-1px 1px 2px #000" ,margin:"-5% 45% 0 45%",fontFamily:"Sacco Condensed",color: "#fdd05a", fontSize:"12vw", letterSpacing:".5vw"}}>
