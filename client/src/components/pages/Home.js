08ba7b756af3a2421f4644f584ac6523aa9bc8c0
Glen Hood <glenhoodatx@gmail.com>
2021-09-06 15:55:03 -0500

    minor styling

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -15,7 +15,7 @@ function Home() {
         
       </Jumbotron>
     {/* <img style={{background:"https://i.imgur.com/zDBO5ot.jpg",margin:"5% 25%",border:"solid #e6dcca ",maxWidth:"600px", width: "100%", maxHeight:"auto"}} className="home-img" src="https://i.imgur.com/cRCyN5v.png"  alt="BandMates logo"></img>  */}
-      <h1 className="about" style={{ textShadow:"-1px 1px 2px #000" ,margin:"-5% 45% 0 45%",fontFamily:"Sacco Condensed",color: "#fdd05a", fontSize:"12vw", letterSpacing:".5vw"}}>
+      <h1 className="about" style={{ textShadow:"-1px 1px 2px #000" ,margin:"5% 45% 0 45%",fontFamily:"Sacco Condensed",color: "#fdd05a", fontSize:"12vw", letterSpacing:".5vw"}}>
         ABOUT
       </h1>
       <p style={{textShadow:"-5px 1px 2px #000",fontFamily:"Menlo",color: "#fdd05a", textAlign:"center", fontSize:"5vw", margin:"3% 10% 10% 10%", letterSpacing:"1vw"}}>A space for musicians to meet other musicians.</p>
